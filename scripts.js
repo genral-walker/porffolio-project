cd05873e8ab465cd83019e0c74bdd748bb7309a2
Lukman Sanni <lukmansanni60@gmail.com>
2020-08-04 05:16:52 +0100

    add additional nav in header

--- scripts.js
+++ scripts.js
@@ -5,4 +5,23 @@ const innerNav = document.querySelector('.header__nav');
 
 nav.addEventListener('click', () => {
     innerNav.classList.toggle('nav-rotate');
+    document.querySelector('.header__bg').classList.toggle('bg-show');
 });
+
+
+const inputName = document.querySelector('#label-1');
+document.querySelector('.form__input-name').addEventListener('focusin', ()=>{
+    inputName.classList.toggle('color-primary');
+});
+document.querySelector('.form__input-name').addEventListener('focusout', ()=>{
+    inputName.classList.remove('color-primary');
+});
+
+const inputMessage = document.querySelector('#label-2');
+document.querySelector('.form__input-message').addEventListener('focusin', ()=>{
+    inputMessage.classList.add('color-primary');
+}); 
+document.querySelector('.form__input-message').addEventListener('focusout', ()=>{
+    inputMessage.classList.remove('color-primary');
+}); 
+ 
