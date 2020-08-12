ef020deb8a154834f166246a3a58c7dd576e9aca
Lukman Sanni <lukmansanni60@gmail.com>
2020-08-12 21:13:11 +0100

    additional nav save for later

--- scripts.js
+++ scripts.js
@@ -10,13 +10,16 @@ nav.addEventListener('click', () => {
 
 
 
-////// For form Label Color Change //////
+////// From Label Color Change //////
 const inputName = document.querySelector('#label-1');
 document.querySelector('.form__input-name').addEventListener('focusin', () => {
-    inputName.classList.toggle('color-primary');
+    inputName.classList.add('color-primary');
 });
 document.querySelector('.form__input-name').addEventListener('focusout', () => {
-    inputName.classList.remove('color-primary');
+    setTimeout(() => {
+        inputName.classList.remove('color-primary');
+        }, 700);
+    
 });
 
 const inputMessage = document.querySelector('#label-2');
@@ -24,7 +27,28 @@ document.querySelector('.form__input-message').addEventListener('focusin', () =>
     inputMessage.classList.add('color-primary');
 });
 document.querySelector('.form__input-message').addEventListener('focusout', () => {
-    inputMessage.classList.remove('color-primary');
+    setTimeout(() => {
+        inputMessage.classList.remove('color-primary');
+        }, 700);
 });
+
+
+
+
+////// Page Section Navigate DropDown //////
+document.querySelector('.header__page-navigate').addEventListener('click', () => {
+    document.querySelector('.header__page-navigate').style.animation = 'none';
+    document.querySelector('.dropdown__ul').classList.toggle('nav-drop');
+});
+
+for (let i = 0; i < 5; i++) {
+    document.querySelectorAll('.dropdown__li')[i].addEventListener('click', () => {
+        setTimeout(() => {
+            document.querySelector('.dropdown__ul').classList.remove('nav-drop');
+        }, 2500);
+    });
+    
+}
+
 
 
