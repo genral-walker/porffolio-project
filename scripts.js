a00c1691c450049ad0f43292a218f646d45136e0
Lukman Sanni <lukmansanni60@gmail.com>
2020-08-31 21:47:23 +0100

    refactor scripts and media queries

--- scripts.js
+++ scripts.js
@@ -1,19 +1,21 @@
 
-////// Page Section Navigate DropDown //////
-document.querySelector('.header__page-navigate').addEventListener('click', () => {
-    document.querySelector('.header__page-navigate').style.animation = 'none';
-    document.querySelector('.dropdown__ul').classList.toggle('nav-drop');
+////// Page Section Navigate DropDown Effects //////
+const dropDownButton = document.querySelector('.header__page-navigate');
+const dropDownItem = document.querySelector('.dropdown__ul');
+const dropDownLists = document.querySelectorAll('.dropdown__li');
+
+dropDownButton.addEventListener('click', () => {
+    dropDownButton.style.animation = 'none';
+    dropDownItem.classList.toggle('nav-drop');
 });
 
-for (let i = 0; i < 5; i++) {
-    document.querySelectorAll('.dropdown__li')[i].addEventListener('click', () => {
+dropDownLists.forEach(list => {
+    list.addEventListener('click', () => {
         setTimeout(() => {
-            document.querySelector('.dropdown__ul').classList.remove('nav-drop');
+            dropDownItem.classList.remove('nav-drop');
         }, 2500);
-    });
-    
-}
-
+    })
+});
 
 ////// From Label Color Change on Focus //////
 const inputName = document.querySelector('#label-1');
@@ -23,8 +25,8 @@ document.querySelector('.form__input-name').addEventListener('focusin', () => {
 document.querySelector('.form__input-name').addEventListener('focusout', () => {
     setTimeout(() => {
         inputName.classList.remove('color-primary');
-        }, 700);
-    
+    }, 700);
+
 });
 
 const inputMessage = document.querySelector('#label-2');
@@ -34,5 +36,5 @@ document.querySelector('.form__input-message').addEventListener('focusin', () =>
 document.querySelector('.form__input-message').addEventListener('focusout', () => {
     setTimeout(() => {
         inputMessage.classList.remove('color-primary');
-        }, 700);
+    }, 700);
 });
